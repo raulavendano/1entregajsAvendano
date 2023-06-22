d59f269ad8a3bbee4bc601d7d3f5dfbafe7e2df9
raulaven <avendanoraul885@gmail.com>
2023-06-22 12:03:00 -0300

    primera entrega

---
+++ index.js
@@ -0,0 +1,13 @@
+let ingreseNombre = prompt ("Ingrese Nombre: ")
+let ingreseApellido = prompt ("Ingrese Apellido: ")
+alert("Bienvenido " +" " + ingreseNombre +" " + ingreseApellido )
+
+let numero = Number(prompt("Ingrese un Numero: "))
+for (let index = 1 ; index < 10; index++) {
+    numero =  numero + index
+    console.log(numero)
+}
+
+function multiplicar(num1, numb2) {
+    
+}
